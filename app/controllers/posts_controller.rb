8748162d17f600324492d6b3784a17917f9c0a3b
Hanna Lee <mani21na@gmail.com>
2020-01-27 02:45:39 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,8 @@ class PostsController < ApplicationController
   # add create method here
   def create
     post = Post.new
-    post.title = param
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
   end 
 end
